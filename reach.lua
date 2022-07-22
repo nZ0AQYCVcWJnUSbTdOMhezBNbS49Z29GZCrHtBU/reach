53e7c4f6fd648000c2f340525487a5f0fa40aa89
nZ0AQYCVcWJnUSbTdOMhezBNbS49Z29GZCrHtBU <109120813+nZ0AQYCVcWJnUSbTdOMhezBNbS49Z29GZCrHtBU@users.noreply.github.com>
2022-07-22 21:38:20 +0200

    Create reach.lua

---
+++ reach.lua
@@ -0,0 +1,97 @@
+local active = true
+local trueActive = true
+local reachType = "Sphere"
+local dmgEnabled = true
+local visualizerEnabled = false
+local ReachSize = 13
+local visualizer = Instance.new("Part")
+visualizer.BrickColor = BrickColor.Blue()
+visualizer.Transparency = 0.6
+visualizer.Anchored = true
+visualizer.CanCollide = false
+visualizer.Size = Vector3.new(0.5,0.5,0.5)
+visualizer.BottomSurface = Enum.SurfaceType.Smooth
+visualizer.TopSurface = Enum.SurfaceType.Smooth
+
+local plr = game.Players.LocalPlayer
+
+local function onHit(hit,handle)
+    local victim = hit.Parent:FindFirstChildOfClass("Humanoid")
+	    if victim and victim.Parent.Name ~= game.Players.LocalPlayer.Name then
+		if dmgEnabled then
+	        for _,v in pairs(hit.Parent:GetChildren()) do
+	            if v:IsA("Part") then
+	                firetouchinterest(v,handle,0)
+	                firetouchinterest(v,handle,1)
+	            end
+			end
+		else
+			firetouchinterest(hit,handle,0)
+			firetouchinterest(hit,handle,1)
+		end
+    end
+end
+
+local function getWhiteList()
+    local wl = {}
+    for _,v in pairs(game.Players:GetPlayers()) do
+        if v ~= plr then
+            local char = v.Character
+            if char then
+                for _,q in pairs(char:GetChildren()) do
+                    if q:IsA("Part") then
+                        table.insert(wl,q)
+                    end
+                end
+            end
+        end
+    end
+    return wl
+end
+
+game:GetService("RunService").RenderStepped:connect(function()
+    if not active or not trueActive then return end
+	local s = plr.Character and plr.Character:FindFirstChildOfClass("Tool")
+	if not s then visualizer.Parent = nil end
+    if s then
+        local handle = s:FindFirstChild("Handle") or s:FindFirstChildOfClass("Part")
+		if handle then
+			if visualizerEnabled then
+				visualizer.Parent = workspace
+			else
+				visualizer.Parent = nil
+			end
+			local reach = tonumber(ReachSize)
+			if reach then
+				if reachType == "Sphere" then
+					visualizer.Shape = Enum.PartType.Ball
+					visualizer.Size = Vector3.new(reach,reach,reach)
+					visualizer.CFrame = handle.CFrame
+		            for _,v in pairs(game.Players:GetPlayers()) do
+		                local hrp = v.Character and v.Character:FindFirstChild("HumanoidRootPart")
+		                if hrp and handle then
+		                    local mag = (hrp.Position-handle.Position).magnitude
+		                    if mag <= reach then
+		                        onHit(hrp,handle)
+		                    end
+		                end
+					end
+				elseif reachType == "Line" then
+					local origin = (handle.CFrame*CFrame.new(0,0,-2)).p
+		    		local ray = Ray.new(origin,handle.CFrame.lookVector*-reach)
+					local p,pos = workspace:FindPartOnRayWithWhitelist(ray,getWhiteList())
+					visualizer.Shape = Enum.PartType.Block
+					visualizer.Size = Vector3.new(1,0.8,reach)
+					visualizer.CFrame = handle.CFrame*CFrame.new(0,0,(reach/2)+2)
+		    		if p then
+		    		    onHit(p,handle)
+		    		else
+		    		    for _,v in pairs(handle:GetTouchingParts()) do
+		    		        onHit(v,handle)
+		    		    end
+		    		end
+				end
+			end
+        end
+    end
+end)
